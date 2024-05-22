6c27b2839f05a111eb4e3ed1855059c89a4fdce1
Francisco Lucas <franciscolucas686@gmail.com>
2024-05-22 19:53:56 -0300

    Criando a seção de planos

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const gulp = require('gulp');
 const sass =  require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin')
 
-function styles(){
+function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
 
-function images(){
+function images() {
     return gulp.src('./src/images/**/*', { encoding: false })
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
